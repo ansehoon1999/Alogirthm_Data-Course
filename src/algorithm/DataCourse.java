8541e28eb208118c6c92aacd6bf43e8543fb7234
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2020-07-11 17:00:22 +0900

    Add files via upload

---
+++ src/algorithm/DataCourse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithm;
+
+/**
+ *
+ * @author PC
+ */
+public class DataCourse {
+    DataCourse() {
+           new first_page().setVisible(true);
+          
+           
+    }
+    public static void main(String[] args) {
+     new DataCourse();
+    }
+}
